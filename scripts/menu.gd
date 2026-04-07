7644c5fdbe58d80cd25fda66a949edc593a16590
lulrp30 <Repicazo@s.sfusd.edu>
2026-04-07 10:58:16 -0700

    2DPlatformer-C4

---
+++ scripts/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _on_p_laybutton_pressed() -> void:
+	PlayerStats.score = 0 
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_quitbutton_pressed() -> void:
+	get_tree().quit()
